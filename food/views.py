339df612cedbc4b0bff261449be61adbbd2b104b
Muhammedfasil <ptfasil786@gmail.com>
2022-11-10 20:37:33 +0530

    Bug fixing

--- food/views.py
+++ food/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.http import Http404
+from django.conf import settings
 
 from .models import Foods
 from .serializers import FoodSerializers
@@ -20,12 +25,19 @@ class SearchProduct(APIView):
 
     def res(self,**pfilter):
         return_data = {}
-        product = Foods.objects.filter(**pfilter)
+        product = Foods.objects.filter(**pfilter).order_by('-id')
+        # Pagination
+        try:
+            if 'page' in self.request.query_params:
+                page_number = self.request.query_params.get('page', 1)
+                product = Paginator(product , settings.PAGINATION_COUNT).page(page_number)
+        except EmptyPage:
+            raise Http404()
+
         fs = FoodSerializers(product,many=True)
-        fdata = fs.data
         return_data['status'] = 'success'
         return_data['results'] = [
-            fdata
+            fs.data
         ]
         return Response(return_data)
 
